38d639f5063697eb732a4ef50f8516bd57e3f485
Worawit Werapan <werapan@go.buu.ac.th>
2023-11-27 21:14:02 +0700

    Add sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'db',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
-      entities: [],
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
       synchronize: true,
       autoLoadEntities: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
